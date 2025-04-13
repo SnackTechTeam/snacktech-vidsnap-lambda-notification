9dd77f3a990049e6941430876522d31d03f985f6
Rafael <rafael.dgs.1993@gmail.com>
2025-04-13 14:53:10 -0300

    adicionando terraform

---
+++ terraform/lambda.tf
@@ -0,0 +1,31 @@
+
+resource "aws_lambda_permission" "allow_sqs" {
+  statement_id  = "AllowExecutionFromSQS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "sqs.amazonaws.com"
+  source_arn    = var.sqsQueueArn
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
+  event_source_arn = var.sqsQueueArn
+  function_name     = aws_lambda_function.lambda.arn
+  batch_size        = 1
+}
+
+
+resource "aws_lambda_function" "lambda" {
+  filename         = "${path.module}/lambda_function.zip"
+  function_name    = "vidsnap-notification"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "lambda_function.handler"
+  runtime          = "python3.10"
+  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
+
+  environment {
+    variables = {
+      MAIL_USER = var.mailUser
+      MAIL_PASS = var.mailPass
+    }
+  }
+}
